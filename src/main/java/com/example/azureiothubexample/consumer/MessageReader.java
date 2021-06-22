1cf6a219540f694fd9a69bc5a8dd76bdc402dced
lahmxu <lahmxu@dfocuspace.com>
2021-06-22 11:08:54 +0800

    fix: commit code

--- src/main/java/com/example/azureiothubexample/consumer/MessageReader.java
+++ src/main/java/com/example/azureiothubexample/consumer/MessageReader.java
@@ -14,12 +14,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 /**
  * receive events from Event Hubs sent from an IoT Hub device.
  */
@@ -28,38 +22,27 @@ import java.util.concurrent.Executors;
 @Order(value = 2)
 public class MessageReader implements CommandLineRunner {
 
-    // 需要将这个对象交给 Spring 容器管理，不定义成员变量这个异步函数会停止接收数据
-    private EventHubConsumerAsyncClient eventHubConsumerAsyncClient;
-
     private static final String EH_COMPATIBLE_CONNECTION_STRING_FORMAT = "Endpoint=%s/;EntityPath=%s;SharedAccessKeyName=%s;SharedAccessKey=%s";
-
-    private static final Map<String, EventHubConsumerAsyncClient> map = new ConcurrentHashMap<>();
-
-    ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     /**
      * CommandLineRunner 启动后执行
+     *
      * @param args
      * @throws Exception
      */
     @Override
     public void run(String... args) throws Exception {
         AzureIotHubConfig config = new AzureIotHubConfig();
-
         // 手动设置参数
-        config.setEventHubsCompatibleEndpoint("");
-        config.setEventHubsCompatiblePath("");
-        config.setIotHubSasKey("");
-        config.setIotHubSasKeyName("");
-        config.setIotHubName("");
+//        config.setEventHubsCompatibleEndpoint("");
+//        config.setEventHubsCompatiblePath("");
+//        config.setIotHubSasKey("");
+//        config.setIotHubSasKeyName("");
+//        config.setIotHubName("");
         start(config);
     }
 
     public void start(AzureIotHubConfig config) throws Exception {
-
-        if (Objects.nonNull(map.get(config.getIotHubName()))) {
-            return;
-        }
 
         String eventHubCompatibleConnectionString = String.format(EH_COMPATIBLE_CONNECTION_STRING_FORMAT,
                 config.getEventHubsCompatibleEndpoint(), config.getEventHubsCompatiblePath(), config.getIotHubSasKeyName(), config.getIotHubSasKey());
@@ -68,37 +51,25 @@ public class MessageReader implements CommandLineRunner {
                 .consumerGroup(EventHubClientBuilder.DEFAULT_CONSUMER_GROUP_NAME)
                 .connectionString(eventHubCompatibleConnectionString);
         try {
-            eventHubConsumerAsyncClient = eventHubClientBuilder.buildAsyncConsumerClient();
+            EventHubConsumerAsyncClient eventHubConsumerAsyncClient = eventHubClientBuilder.buildAsyncConsumerClient();
             receiveFromAllPartitions(eventHubConsumerAsyncClient, config);
-            map.put(config.getIotHubName(), eventHubConsumerAsyncClient);
 
         } catch (Exception ex) {
             log.error("[Azure iotHub] build async client error.");
         }
     }
 
-    public void stop(String iotHubName) {
-        EventHubConsumerAsyncClient eventHubConsumerAsyncClient = map.get(iotHubName);
-        if (Objects.nonNull(eventHubConsumerAsyncClient)) {
-            eventHubConsumerAsyncClient.close();
-            map.remove(iotHubName);
-        }
-    }
-
     private void receiveFromAllPartitions(EventHubConsumerAsyncClient eventHubConsumerAsyncClient, AzureIotHubConfig config) {
-        // Create an async consumer client as configured in the builder.
-        executorService.execute(() -> {
-            eventHubConsumerAsyncClient
-                    .receive(false) // set this to false to read only the newly available events
-                    .subscribe(partitionEvent -> {
-                        String receiveBodyStr = partitionEvent.getData().getBodyAsString();
-                        log.info("[Azure iotHub] Received message: {}", receiveBodyStr);
+        eventHubConsumerAsyncClient
+                .receive(false) // set this to false to read only the newly available events
+                .subscribe(partitionEvent -> {
+                    String receiveBodyStr = partitionEvent.getData().getBodyAsString();
+                    log.info("[Azure iotHub] Received message: {}", receiveBodyStr);
 
-                    }, ex -> {
-                        log.error("[Azure iotHub] Error handler message: {}", ex.getMessage());
-                    }, () -> {
-                        log.info("[Azure iotHub] Completed receiving events");
-                    });
-        });
+                }, ex -> {
+                    log.error("[Azure iotHub] Error handler message: {}", ex.getMessage());
+                }, () -> {
+                    log.info("[Azure iotHub] Completed receiving events");
+                });
     }
 }
